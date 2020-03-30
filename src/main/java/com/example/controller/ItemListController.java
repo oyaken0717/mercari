5d9a4179cad25427f704896ae7d1a1afd60ef4c4
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-30 15:09:43 +0900

    【new】ItemRepositoryクラス作成

--- src/main/java/com/example/controller/ItemListController.java
+++ src/main/java/com/example/controller/ItemListController.java
@@ -1,14 +1,30 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Item;
+import com.example.repository.ItemRepository;
+
+/**
+ * 商品情報を表示するコントローラー.
+ * 
+ * @author oyamadakenji
+ *
+ */
 @Controller
 @RequestMapping("")
 public class ItemListController {
 
+	@Autowired
+	public ItemRepository itemRepository;
+	
 	@RequestMapping("")
 	public String toItemList() {
+		List<Item> itemList = itemRepository.findAll();
 		return "list";
 	}
 }
---
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -0,0 +1,42 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Item;
+
+/**
+ * Itemsテーブルの情報を取得するレポジトリ.
+ * 
+ * @author oyamadakenji
+ *
+ */
+@Repository
+public class ItemRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template; 
+	
+	public static final RowMapper<Item> ITEM_ROW_MAPPER = (rs,i) -> {
+		Item item = new Item();
+		item.setId(rs.getInt("id"));
+		item.setName(rs.getString("name"));
+		item.setCondition(rs.getInt("condition"));
+		item.setCategory(rs.getInt("category"));
+		item.setBrand(rs.getString("brand"));
+		item.setPrice(rs.getDouble("price"));
+		item.setShipping(rs.getInt("shipping"));
+		item.setDescription(rs.getString("description"));
+		return item;
+	};
+	
+	public List<Item> findAll() {
+		String sql = "SELECT id, name, condition, category, brand, price, shipping, description FROM items ORDER BY id";
+		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
+		return itemList;
+	}
+}
